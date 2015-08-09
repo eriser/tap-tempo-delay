84bb3b0ec634ba76534f28f54cb8a8c4cdf51ec0
Ryan Foo <ryanfoo@nyu.edu>
2015-08-09 18:10:23 -0400

    Delay Class Structure Added

---
+++ ysDelay/Source/Delay.cpp
@@ -0,0 +1,9 @@
+//
+//  Delay.cpp
+//  ysDelay
+//
+//  Created by Ryan Foo on 8/9/15.
+//
+//
+
+#include "Delay.h"
---
+++ ysDelay/Source/Delay.h
@@ -0,0 +1,69 @@
+//
+//  Delay.h
+//  ysDelay
+//
+//  Created by Ryan Foo on 8/9/15.
+//
+//
+
+#ifndef __ysDelay__Delay__
+#define __ysDelay__Delay__
+
+#include <stdio.h>
+
+#define INIT_SAMPLE_RATE        44100
+
+class Delay {
+    
+public:
+    // Constructor
+    Delay();
+    // Deconstructor
+    ~Delay();
+    
+    // Parameter Data Structure
+    struct Parameters {
+        Parameters() noexcept
+        : delay(5.f),
+        fdbk(0.1f),
+        mod(0.25f),
+        mix(0.5f)
+        {}
+        
+        float delay;
+        float fdbk;
+        float mod;
+        float mix;
+    };
+    
+    // Get Parameters
+    const Parameters& getParameters() const noexcept {
+        return parameters;
+    };
+    
+    // Set Parameters
+    void setParameters(const Parameters& newParams);
+    
+    // Set Sample Rate
+    void setSampleRate(const double sampleRate);
+    
+    // Clear buffers
+    void prepareToPlay();
+    
+    // Audio Processing Methods
+    void processMono(float* const samples, const int numSamples);
+    void processStereo(float* const left, float* const right, const int numSamples);
+    
+private:
+    // Parameters DS
+    Parameters parameters;
+    // Delay Circular Buffers
+    float delayL[1024], delayR[1024];
+    // Delay Circular Buffer Reader/Writer
+    int readerL, readerR, writerL, writerR;
+    // Sample Rate
+    double currentSampleRate;
+    
+};
+
+#endif /* defined(__ysDelay__Delay__) */
